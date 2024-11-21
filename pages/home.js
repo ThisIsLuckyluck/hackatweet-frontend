79926f0e2a656e6e07ec19d5d2ec89e293dc04ae
Olivia <mannerolivia@gmail.com>
2024-11-21 17:47:03 +0100

    Create Home page

---
+++ pages/home.js
@@ -0,0 +1,17 @@
+import Home from '../components/Home';
+import Head from "next/head";
+
+function Index() {
+  return (
+   <>
+    <Head>
+    <title>Hackatweet: Page to login</title>
+    </Head>
+    
+  
+    <Home />
+   </>
+  );
+}
+
+export default Index;
