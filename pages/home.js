c53b845e4fabd4104072297c8270359d7e3a6b45
ThisIsLuckyluck <denislucas1a@gmail.com>
2024-11-22 12:59:00 +0100

    Update pages

--- pages/home.js
+++ pages/home.js
@@ -1,17 +1,7 @@
-import Home from '../components/Home';
-import Head from "next/head";
+import Home from "../components/Home";
 
 function Index() {
-  return (
-   <>
-    <Head>
-    <title>Hackatweet: Page to login</title>
-    </Head>
-    
-  
-    <Home />
-   </>
-  );
+  return <Home />;
 }
 
 export default Index;
--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,7 @@ import Login from "../components/Login";
 import Head from "next/head";
 
 function Index() {
-  return (
-    <>
-      <Head>
-        <title>Hackatweet: Home page to login</title>
-      </Head>
-      <Login />
-    </>
-  );
+  return <Login />;
 }
 
 export default Index;
